7da28b110bc86431c2c4b949fe2be61573101c08
marianabhh <marianabh1409@gmail.com>
2025-11-29 23:41:59 -0600

    Agregar ButtonReader para lectura de botones

--- main.cpp
+++ main.cpp
@@ -52,12 +52,75 @@ private:
     uint8_t count_;
 };
 
+class ButtonReader {
+public:
+    ButtonReader(const uint8_t* pins, uint8_t count, uint16_t debounceMs = 25)
+      : pins_(pins), count_(count), debounceMs_(debounceMs) {
+        for (uint8_t i = 0; i < 4; ++i) {
+            curr_[i] = prev_[i] = HIGH;
+            lastChange_[i] = 0;
+            edge_[i] = false;
+        }
+    }
+
+    void begin() {
+        for (uint8_t i = 0; i < count_; ++i) {
+            pinMode(pins_[i], INPUT_PULLUP);
+            curr_[i] = prev_[i] = digitalRead(pins_[i]);
+            lastChange_[i] = millis();
+            edge_[i] = false;
+        }
+    }
+
+    void update() {
+        unsigned long now = millis();
+        for (uint8_t i = 0; i < count_; ++i) {
+            uint8_t r = digitalRead(pins_[i]);
+            edge_[i] = false;
+            if (r != curr_[i] && (now - lastChange_[i] >= debounceMs_)) {
+                prev_[i] = curr_[i];
+                curr_[i] = r;
+                lastChange_[i] = now;
+                if (prev_[i] == HIGH && curr_[i] == LOW) {
+                    edge_[i] = true;
+                }
+            }
+        }
+    }
+
+    bool isPressed(uint8_t idx) const {
+        return (idx < count_) ? (curr_[idx] == LOW) : false;
+    }
+
+    bool risingEdge(uint8_t idx) const {
+        return (idx < count_) ? edge_[idx] : false;
+    }
+
+    uint8_t anyRisingEdge() const {
+        for (uint8_t i = 0; i < count_; ++i) {
+            if (edge_[i]) return i;
+        }
+        return 0xFF;
+    }
+
+private:
+    const uint8_t* pins_;
+    uint8_t count_;
+    uint16_t debounceMs_;
+    uint8_t curr_[4];
+    uint8_t prev_[4];
+    unsigned long lastChange_[4];
+    bool edge_[4];
+};
+
 // Instancias globales
 
 LEDDriver      leds(LED_PINS, 4);
+ButtonReader buttons(BUTTON_PINS,4);
 
 void setup() {
     leds.begin();
+    buttons.begin();
 }
 
 void loop() {}
